ce468c916c44604fa3f790baadd416c2c2420d82
volyES <qwertqwerqwerqwer777@gmail.com>
2024-06-07 15:46:00 +0300

    [start-project-snippets][#13]

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -29,12 +29,20 @@ class UserReistrationForm(ModelForm):
                    'email': TextInput(attrs={'placeholder': 'Электронная почта'}),
                    }
 
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('Пользователь с таким именем уже существует')
+        if len(username) < 3:
+            raise ValidationError('Имя пользователя слишком короткое')
+        return username
+
     def clean_password(self):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
-        if password1 and password2 and password1 != password2:
-            raise ValidationError('Пароли не совпадают')
-        return password2
+        if (password1 and password2 and (password1 == password2)):
+            return password2
+        raise ValidationError('Пароли не совпадают')
 
     def save(self, commit=True):
         user = super(UserReistrationForm, self).save(commit=False)
